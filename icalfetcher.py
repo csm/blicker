cd6b93b317d18e8848e73d2c09392d485ab4f232
Casey Marshall <casey.s.marshall@gmail.com>
2010-03-07 19:59:47 -0800

    first commit

---
+++ icalfetcher.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+# icalfetcher.py: fetch an iCalendar file, turn it into a JSON file.
+# Copyright (C) 2010 Casey Marshall.
+
+import sys
+import json
+import icalendar
+import urllib2
+import urlparse
+
+def event2dict(event):
+	d = {}
+	for k in event.keys():
+		if k == 'DTSTART':
+			d['begins'] = str(event[k])
+		if k == 'SUMMARY':
+			d['summary'] = str(event[k])
+		if k == 'LOCATION':
+			d['location'] = str(event[k])
+		if k == 'DESCRIPTION':
+			d['description'] = str(event[k])
+	return d
+
+def cal2dict(cal):
+	d = {}
+	for k in cal.keys():
+		if k == 'X-WR-CALNAME':
+			d['name'] = str(cal[k])
+	d['events'] = map(lambda (e): event2dict(e), cal.subcomponents)
+	return d
+
+def main(argv):
+	if len(argv) < 2:
+		print "usage: icalfetcher.py URL [output.json]"
+		return 1
+	url = argv[1]
+	u = urlparse.urlparse(url)
+	if len(argv) >= 3:
+		outfile = argv[2]
+	else:
+		outfile = u.netloc + ".json"
+	req = urllib2.urlopen(url)
+	ical = ''.join(req.readlines())
+	cal = icalendar.Calendar.from_string(ical)
+	d = cal2dict(cal)
+	d['source'] = url
+	d['format'] = 'ical'
+	f = open(outfile, 'w')
+	json.dump(d, f)
+	f.close()
+	return 0
+	
+if __name__ == "__main__":
+	sys.exit(main(sys.argv))
